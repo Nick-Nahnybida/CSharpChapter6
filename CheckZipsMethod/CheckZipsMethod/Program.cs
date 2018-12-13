56ece1ce74a64484cd6d9ac320d60c02c767eba5
Nick-Nahnybida <nicknahnybida@gmail.com>
2018-12-13 13:17:46 -0600

    update

---
+++ CheckZipsMethod/CheckZipsMethod/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace CheckZipsMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input;
+
+            WriteLine("Please enter your zip code");
+            input = Convert.ToInt32(ReadLine());
+            displayZips(input);
+
+        }
+        static void displayZips(int input)
+        {
+            int[] zips = { 11111, 22222, 33333, 44444, 55555, 66666, 77777, 88888, 99999, 10101 };
+            int j = 0;
+            do
+            {
+                bool availableZip = false;
+                
+                for (int i = 0; i < zips.Length; i++)
+                {
+                    if (input == zips[i])
+                    {
+                        availableZip = true;
+                    }
+                }
+                if (availableZip)
+                {
+                    WriteLine("Your zip code is in our delivery area");
+                }
+                else
+                {
+                    WriteLine("Your zip code is not in our delivery area");
+                }
+                j++;
+                
+            } while (j != 1);
+        }
+    }
+}
---
+++ Student/Student/CreateStudents.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Student
+{
+    class CreateStudents
+    {
+        static void Main(string[] args)
+        {
+            Student first = new Student();
+            Student second = new Student();
+            first.IdNumber = 123;
+            first.LastName = "Burton";
+            first.GradePointAverage = 3.5;
+
+            Write("Enter student's last name >>>> ");
+            second.LastName = ReadLine();
+            Write("Enter student's ID number >>>> ");
+            second.IdNumber = Convert.ToInt32(ReadLine());
+
+            Write("Enter student's grade point average >>>> ");
+            second.GradePointAverage = Convert.ToDouble(ReadLine());
+
+            Display(first);
+            Display(second);
+        }
+        static void Display(Student stu)
+        {
+            WriteLine("{0} {1} {2}", stu.IdNumber, stu.LastName, stu.GradePointAverage);
+        }
+    }
+}
