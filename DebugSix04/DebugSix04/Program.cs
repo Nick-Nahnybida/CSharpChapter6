4f0e8ed127c1ffaaf6b2cf98e1cbbf63f58a4b6e
Nick-Nahnybida <nicknahnybida@gmail.com>
2018-12-17 17:56:42 -0600

    update

--- DebugSix04/DebugSix04/Program.cs
+++ DebugSix04/DebugSix04/Program.cs
@@ -11,51 +11,32 @@ namespace DebugSix04
     {
         public static void Main(string[] args)
         {
-            /*const double QUIT = 999;
-            double[] numbers = new double[20];
+            const int QUIT = 999;
+            int[] numbers = new int[20];
             int x = 0;
-            double num = 0;
+            int num;
             double average;
             double total = 0;
             string inString;
-            WriteLine("Please enter a number or " + QUIT + " to quit...");
+            Write("Please enter a number or " +
+               QUIT + " to quit...");
             inString = ReadLine();
-            num = Convert.ToDouble(inString);
-            while ((x < numbers.Length) && num == QUIT)
+            num = Convert.ToInt32(inString);
+            while ((x < numbers.Length) && num != QUIT)
             {
                 numbers[x] = num;
                 total += numbers[x];
-                x++;
-                Write("Please enter a number or " + QUIT + " to quit...");
+                ++x;
+                Write("Please enter a number or " +
+                   QUIT + " to quit...");
                 inString = ReadLine();
                 num = Convert.ToInt32(inString);
             }
             WriteLine("The numbers are:");
-            for (int y = 0; y < x; x++)
-                WriteLine("{0,19}", numbers[y]);
-
+            for (int y = 0; y < x; ++y)
+                Write("{0,6}", numbers[y]);
             average = total / x;
             WriteLine();
-            WriteLine("The average is {0}", average);*/
-
-            int[] arr = new int[20];
-            int i;
-            double average;
-            double total = 0;
-            
-
-            WriteLine("Input up to 20 values in the array");
-            for (i = 0; i < 20; i++)
-            {
-                Write("Enter a number - " + i + " << ");
-                arr[i] = Convert.ToInt32(ReadLine());
-            }
-            
-            for (i = 0; i < arr.Length; i++)
-            {
-                total = arr[i] + total;
-            }
-            average = total / arr.Length;
             WriteLine("The average is {0}", average);
         }
     }
